5c7bec679f527c55635de6af638535deb476df4c
DIOGO BESEN <diogoandrebesen@yahoo.com.br>
2012-11-14 20:29:52 -0200

    primeiro commit

--- exercicio5.php
+++
@@ -1,5 +0,0 @@
-<?php
-/*5.	Criar uma função chamada multiplica() e refazer o exercício 4d utilizando essa função.*/
-?>
-
-
---
+++ exercicios/exercicio5.php
@@ -0,0 +1,41 @@
+<?php
+/*5.	Criar uma função chamada multiplica() e refazer o exercício 4d utilizando essa função.
+d) Escrever um algoritmo que multiplique os números utilizando apenas o operador +, ex: (3 * 5) = 5 + 5 + 5*/
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="utf-8"/>
+<title> Numero 5</title>
+</head>
+<body>
+
+<?php
+function multiplica ($n1, $n2) {
+
+			$n1 = 3;
+			$n2 = 5;
+			$count = 0;
+
+			for ($n2 : $n1--) {
+			echo count =  n2 + n2;
+			}
+
+			return count(var);
+
+}
+
+
+?>
+
+
+
+
+
+
+
+
+</body>
+</html>
